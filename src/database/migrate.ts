6f7d25a521395c9de458d999e64ec0a2e7c26006
luardino <luardino@icloud.com>
2026-02-12 01:12:24 +0100

    Add SQLite database struture

---
+++ src/database/migrate.ts
@@ -0,0 +1,31 @@
+import { SQLiteDatabase } from "expo-sqlite";
+
+export async function migrate(database: SQLiteDatabase) {
+    await database.execAsync(`
+        PRAGMA foreign_keys = ON;
+
+        CREATE TABLE IF NOT EXISTS "targets" (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+            "name" TEXT NOT NULL,
+            "amount" FLOAT NOT NULL,
+            "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+
+        CREATE TABLE IF NOT EXISTS "transactions" (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+            "target_id" INTEGER NOT NULL,
+            "amount" FLOAT NOT NULL,
+            "observation" TEXT NULL,
+            "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+
+            CONSTRAINT "fk_targets_transactions" 
+            FOREIGN KEY ("target_id") REFERENCES "targets"("id") AdON DELETE CASCADE
+
+        );
+        `)
+
+
+}
